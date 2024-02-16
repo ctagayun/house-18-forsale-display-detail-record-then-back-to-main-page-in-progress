e92257b31de4d69dba045a5caa9ea68dfe5a3d30
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-02-16 16:41:27 -0600

    Added comments to: index.html and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min"
 
+/*=======================================================
+//  The main.jsx is responsible in instantiating app.jsx
+//  The App.jsx is instantiated in index.html under 
+//  the element: id attribute equals "root"
+=========================================================*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
